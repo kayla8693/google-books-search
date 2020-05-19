0bf18870edb3f28c61b4acd9d6518c7ea2723377
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-19 16:10:40 -0500

    added grid component

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function SearchBar() {
     return (
         <form noValidate autoComplete="off">
             <TextField id="outlined-search" label="Search" type="search" variant="outlined" />
+            <Button
+            variant="contained" color="primary"
+            type="button"
+            value="submit"
+            id="submit">Submit
+            </Button>
         </form>
     )
 };
