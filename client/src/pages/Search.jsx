7b07f588c7d821708faa487e442efe3c7009292a
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-05-19 16:27:15 -0500

    added search layout

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,1 +1,35 @@
 import React from "react";
+
+import { Container, Row, Col } from "../components/Grid";
+import Image from "../components/Image";
+
+export function BookList({ children }) {
+    return <ul className="list-group">{children}</ul>;
+}
+
+export function BookListItem({
+    title,
+    authors,
+    description,
+    image,
+    link
+}) {
+    return (
+        <li className="list-group-item">
+            <Container>
+                <Row>
+                    <Col size="xs-4 sm-2">
+                        <Image src={image || "https://placehold.it/300x300"} />
+                    </Col>
+                    <Col size="xs-8 sm-9">
+                        <h3>{title} by {authors}</h3>
+                        <p>{description}</p>
+                        <a rel="noreferrer noopener" target="blank" href={link}>
+                            Go to Book!
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </li>
+    );
+}
